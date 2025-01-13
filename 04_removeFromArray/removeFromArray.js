e861e57cc6cce063dc9305a6166bc1405304fffc
dominiquetoronto <komnatskyy@gmail.com>
2025-01-13 14:40:44 +0000

    5th commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+    
+    let staying = []; 
+    for( let i = 0; i < array.length; i++){
+        if(!valuesToRemove.includes(array[i])) {
+            staying.push(array[i])
+        }
+    }
+    return staying;
 };
 
 // Do not edit below this line

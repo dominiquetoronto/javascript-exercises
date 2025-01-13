820b906128801751250ed7e799a2680f64a166dc
dominiquetoronto <komnatskyy@gmail.com>
2025-01-13 13:59:13 +0000

    3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
+const reverseString = function(text) {
+    const array = text.split('');
+    let reversed = '';
+    for (i = array.length - 1; i >= 0; i--){
+        reversed += array[i];
+    } return reversed;
+}
+        
+    
 
 // Do not edit below this line
 module.exports = reverseString;

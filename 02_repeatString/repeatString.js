87977edb9679ac1de0bc719e4c03cf92567a1e0f
dominiquetoronto <komnatskyy@gmail.com>
2025-01-13 10:38:40 +0000

    2nd commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'; // Handle negative numbers
+    }
+    let result = '';
+    for(let i = 0; i < num; i++) {
+        result += string; // Concatanate string to result
+    } 
+    return result; // Return result after the loop
+    };
 
 // Do not edit below this line
 module.exports = repeatString;

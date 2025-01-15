09d8555d97277ae84f9dc5997309df4b4fee3fbd
dominiquetoronto <komnatskyy@gmail.com>
2025-01-15 15:46:11 +0000

    tempConversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9;
+  let celsiusFinal = +celsius.toFixed(1);
+  return celsiusFinal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = celsius * 9/5 + 32;
+  let fahrenheitFinal = +fahrenheit.toFixed(1);
+  return fahrenheitFinal;
 };
 
 

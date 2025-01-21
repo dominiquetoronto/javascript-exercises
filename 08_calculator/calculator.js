7213596999df7bc794328579d14a75ea7631c7cf
dominiquetoronto <komnatskyy@gmail.com>
2025-01-21 12:58:11 +0000

    final subtask

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -22,12 +22,21 @@ const multiply = function(numbersArray) {
     return multnum;
 };
 
-const power = function(a, b) {
-	return a ** b; 
+const power = function(num1, num2) {
+	return num1 ** num2; 
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  if(num === 0 || num === 1){
+    return 1;
+  } else { 
+    for(let i = num; i > 1; i--){
+      fact *= i; 
+    }
+  } return fact
+}
+ 
 };
 
 // Do not edit below this line
